d37785b809befb44d6fe2d84615ad80f5a5a2085
fuaus <kotingfunk@gmail.com>
2022-08-16 19:00:07 -0400

    homeroutes edits and change project.handlebars to plant.handlebars

---
+++ controllers/api/commentRoutes.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+const { User, Plant, Comment } = require('../../models');
+
+router.get('/', async (req, res)=>{
+    try {
+        
+    }
+    catch (err) {
+        
+    }
+});
+
+
+
+
+module.exports = router;
