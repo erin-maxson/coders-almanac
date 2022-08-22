b37629754d96545e34790f33e4510ab78284ce8b
erin-maxson <erin@cohatch.com>
2022-08-22 18:25:48 -0400

    login page formatting (still isn't quite right)

--- public/js/login.js
+++ public/js/login.js
@@ -46,22 +46,6 @@ const signupFormHandler = async (event) => {
   }
 };
 
-// from foundation element for sign up form -- EM
-// $(function () {
-//   var showClass = 'show';
-
-//   $('input').on('checkval', function () {
-//     var label = $(this).prev('label');
-//     if(this.value !== '') {
-//       label.addClass(showClass);
-//     } else {
-//       label.removeClass(showClass);
-//     }
-//   }).on('keyup', function () {
-//     $(this).trigger('checkval');
-//   });
-// });
-
 document
   .querySelector('.login-form')
   .addEventListener('submit', loginFormHandler);
