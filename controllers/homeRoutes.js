3ddc3797461e29a0dbefd17c890afc703ef3fe8c
fuaus <kotingfunk@gmail.com>
2022-08-22 21:56:40 -0400

    changes to homeRoutes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -75,26 +75,26 @@ router.get('/plants/:id', withAuth, async (req, res) => {
   }
 });
 
-router.get('/plantdetails/:id', withAuth, async (req, res) => {
-  try {
-    const plantData = await Plant.findByPk(req.params.id, {
-      include: [
-        User, {
-          model: Comment, include: [User]
-        }
-      ],
-    });
+// router.get('/plantdetails/:id', withAuth, async (req, res) => {
+//   try {
+//     const plantData = await Plant.findByPk(req.params.id, {
+//       include: [
+//         User, {
+//           model: Comment, include: [User]
+//         }
+//       ],
+//     });
 
-    const plant = plantData.get({ plain: true });
-    // the 'plantdetails', will need to be in the handlebar file name
-    res.render('plantdetails', {
-      ...plant,
-      logged_in: req.session.logged_in
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
+//     const plant = plantData.get({ plain: true });
+//     // the 'plantdetails', will need to be in the handlebar file name
+//     res.render('plantdetails', {
+//       ...plant,
+//       logged_in: req.session.logged_in
+//     });
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
 
 // Use withAuth middleware to prevent access to route
 // this is the route for form.handlebars
